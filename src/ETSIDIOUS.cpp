9696902bcc9aac2a3b2c853978b69736f06683a9
davidherguetasoto <david.hergueta.soto@alumnos.upm.es>
2021-05-15 20:10:20 +0200

    PRUEBA

--- src/ETSIDIOUS.cpp
+++ src/ETSIDIOUS.cpp
@@ -4,9 +4,4 @@ using namespace std;
 void main()
 {
 	cout << "Hola mundo de miada" << endl;
-	cout << "Space invaders" << endl;
-	cout << "hola que tal no estamos" << endl;
-	int pollaas;
-	cout << "hergueta cagon" << endl;
-	cout << "tu puta madre" << endl;
 }
