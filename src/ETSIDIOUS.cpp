30413359c8dd02dbe0654ddb6396ac8c8300fa13
davidherguetasoto <david.hergueta.soto@alumnos.upm.es>
2021-05-15 20:02:21 +0200

    SUBCARPETAS

---
+++ src/ETSIDIOUS.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+void main()
+{
+	cout << "Hola mundo de miada" << endl;
+	cout << "Space invaders" << endl;
+	cout << "hola que tal no estamos" << endl;
+	int pollaas;
+	cout << "hergueta cagon" << endl;
+	cout << "tu puta madre" << endl;
+}
