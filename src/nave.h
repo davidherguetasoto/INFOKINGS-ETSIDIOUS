6b3f91e488c523d7c8f6a03703cb07cf6852dc53
davidherguetasoto <david.hergueta.soto@alumnos.upm.es>
2021-05-22 17:51:49 +0200

    Primera version clase nave

--- src/nave.h
+++ src/nave.h
@@ -2,6 +2,13 @@
 #include "ObjetoMovil.h"
 class Nave : public ObjetoMovil
 {
-
+protected:
+	float vida; //Vida de las naves
+	//provisional
+	float radio;
+	float altura;
+public:
+	void Dibuja();
+	//añadir funcion para disparo
 };
 
