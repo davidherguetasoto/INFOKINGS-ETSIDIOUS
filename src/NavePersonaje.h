17764e5a467473e402c0d03d6f7ad5b448f86e39
davidherguetasoto <david.hergueta.soto@alumnos.upm.es>
2021-06-11 18:04:04 +0200

    .

--- src/NavePersonaje.h
+++ src/NavePersonaje.h
@@ -1,13 +1,22 @@
 #pragma once
 #include "Nave.h"
 #include "ColorRGB.h"
+
 class NavePersonaje :public Nave
 {
+public:
+	//prueba bonus
+	void IncrementaNumBonus();
+	void SetNumBonuns(int num);
+	int GetNumBonus();
+	//prueba bonus
 protected:
 	//PROVISIONAL PARA HACER DIBUJOS DE PRUEBA
 	float altura;
 	float radio;
 	ColorRGB color;
+	//prueba bonus
+	int num_bonus;
 public:
 	NavePersonaje();
 	void dibuja(); //Define cómo se dibuja el personaje
