1933aa1368544fcc95418ca1b53e547b264db040
luis.torres.delnuevo@alumnos.upm.es <luis.torres.delnuevo@alumnos.upm.es>
2021-06-15 01:15:19 +0200

    bonus

--- src/Mundo.cpp
+++ src/Mundo.cpp
@@ -26,6 +26,20 @@ void Mundo::inicializa()
 	nivel = 0;
 	cargarNivel();
 
+	Bonus* d = new BonusMisiles;
+	d->setPos(6, 12);
+	bonus.agregar(d);
+	Bonus* c = new BonusDisparoDoble;
+	c->setPos(-6, 15);
+	bonus.agregar(c);
+	Bonus* a = new BonusPuntExtras;
+	a->setPos(-6, 12);
+	bonus.agregar(a);
+	Bonus* b = new BonusVidas;
+	b->setPos(6, 15);
+	bonus.agregar(b);
+
+	/*
 	int nrandom1;
 	int nrandom2;
 	nrandom1 = 1 + rand() % 2;
@@ -56,7 +70,7 @@ void Mundo::inicializa()
 			a->setPos(-6, 12);
 			bonus.agregar(a);
 		}
-	}
+	}*/
 }
 void Mundo::dibuja()
 {
