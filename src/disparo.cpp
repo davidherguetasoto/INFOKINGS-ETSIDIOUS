e36140487944b10b71d82a76678b9e6bd5fba882
davidherguetasoto <david.hergueta.soto@alumnos.upm.es>
2021-06-16 14:11:28 +0200

    Eliminación de cosas que no se utilizan en la clase disparo

--- src/disparo.cpp
+++ src/disparo.cpp
@@ -9,19 +9,6 @@ Disparo::~Disparo()
 	radio = 0.5f;
 	altura = 0.5f;
 }
-Vector2D Disparo::getOrigen()
-{
-	return origen;
-}
-void Disparo::setOrigen(Vector2D ori)
-{
-	origen = ori;
-}
-void Disparo::setOrigen(float ix,float iy)
-{
-	origen.x = ix;
-	origen.y = iy;
-}
 float Disparo::getDano()
 {
 	return daño;
--- src/disparo.h
+++ src/disparo.h
@@ -1,8 +1,6 @@
 //CLASE ABSTRACTA PARA LA DEFINICIÓN DE LOS DISPAROS
 #pragma once
 #include "ObjetoMovil.h"
-#include "ColorRGB.h"
-#include "Vector2D.h"
 
 //TIPOS DE DISPAROS DEL JUEGO
 #define NINGUNO -1
@@ -16,7 +14,6 @@ class Disparo:public ObjetoMovil
 {
 protected:
 	float daño; //Daño que produce el disparo
-	Vector2D origen; //Origen del disparo
 	int tipo; //Atributo para seleccionar los distintos tipos de disparo
 	float radio;
 	float altura;
@@ -24,9 +21,6 @@ public:
 	Disparo(float daño,int tipo);
 	virtual ~Disparo();
 	virtual void dibuja()=0;
-	Vector2D getOrigen();
-	void setOrigen(Vector2D ori);
-	void setOrigen(float ix, float iy);
 	float getDano();
 	int getTipo();
 	float getRadio() { return radio; }
